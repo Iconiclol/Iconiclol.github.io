1e6b45524db5247e0f8e040a643e043f49f00872
Iconiclol <101067401+Iconiclol@users.noreply.github.com>
2024-05-25 22:18:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
+let startTime = localStorage.getItem('startTime');
+if (!startTime) {
+    startTime = new Date();
+    startTime.setHours(startTime.getHours() - 0);
+    localStorage.setItem('startTime', startTime);
+}
+
+function updateDaysCounter() {
+    const now = new Date();
+    const timeDiff = now - new Date(startTime);
+
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    document.getElementById('daysCounter').innerText = `${days} Days ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+updateDaysCounter();
+
+setInterval(updateDaysCounter, 1000);
+
+// Box and line around cursor
+const boxOutline = document.querySelector('.box-outline');
+const line = document.querySelector('.line');
+
 document.addEventListener('mousemove', (e) => {
-    const boxOutline = document.querySelector('.box-outline');
-    const box = document.querySelector('.box');
-    const line = document.querySelector('.line');
-    
-    const boxWidth = box.offsetWidth;
-    const boxHeight = box.offsetHeight;
+    boxOutline.style.left = e.pageX - 50 + 'px';
+    boxOutline.style.top = e.pageY - 50 + 'px';
+    line.style.left = e.pageX + 2 + 'px';
+    boxOutline.style.display = 'block';
+    line.style.display = 'block';
+});
 
-    box.style.left = `${e.clientX - (boxWidth / 2)}px`;
-    box.style.top = `${e.clientY - (boxHeight / 2)}px`;
-    boxOutline.style.left = `${e.clientX - (boxWidth / 2)}px`;
-    boxOutline.style.top = `${e.clientY - (boxHeight / 2)}px`;
-
-    const lineLeft = e.clientX + (boxWidth / 2); // Adjusting the line position to the right of the box
-    const lineTop = e.clientY - (boxHeight / 2);
-    line.style.left = `${lineLeft}px`;
-    line.style.top = `${lineTop}px`;
+document.addEventListener('mouseleave', () => {
+    boxOutline.style.display = 'none';
+    line.style.display = 'none';
 });
